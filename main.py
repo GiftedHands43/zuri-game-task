b1c47afb615a5f71f54839f7090fc76f895eedf0
GiftedHands43 <100702359+GiftedHands43@users.noreply.github.com>
2022-06-10 13:26:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ while True:
        possible_actions = ["R", "P", "S"]
        computer_action = random.choice(possible_actions)
        print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+       
+       if user action not in options:
+           print("Invalid choice!\nEnter a valid option\n")
+           continue
 
-       if user_action == computer_action:
-           print(f"Both players selected {user_action}. It's a tie!")
+       else:
+           if user_action == computer_action:
+               print(f"Both players selected {user_action}. It's a tie!")
 
-       elif user_action == "R":
-           if computer_action == "S":
-               print("Rock smashes scissors! You win!")
+           elif user_action == "R":
+               if computer_action == "S":
+                   print("Rock smashes scissors! You win!")
 
-           else:
-               print("Paper covers rock! You lose.")
+               else:
+                   print("Paper covers rock! You lose.")
 
-       elif user_action == "P":
-           if computer_action == "R":
-               print("Paper covers rock! You win!")
+           elif user_action == "P":
+               if computer_action == "R":
+                   print("Paper covers rock! You win!")
 
-           else:
-               print("Scissors cuts paper! You lose.")
+               else:
+                   print("Scissors cuts paper! You lose.")
 
-       elif user_action == "S":
-           if computer_action == "P":
-               print("Scissors cuts paper! You win!")
+           elif user_action == "S":
+               if computer_action == "P":
+                   print("Scissors cuts paper! You win!")
 
-           else:
-               print("Rock smashes scissors! You lose.")
+               else:
+                   print("Rock smashes scissors! You lose.")
 
        play_again = input("Play again? (y/n): ")
        if play_again.lower() != "y":
